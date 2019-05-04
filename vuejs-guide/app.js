f86ac5e1b21453db2bdbc66c43d8b5b6b8473652
deepwelloper <deepwelloper@gmail.com>
2019-05-04 18:16:13 +0900

    컴포넌트를 사용한 작성방법

--- vuejs-guide/app.js
+++ vuejs-guide/app.js
@@ -48,3 +48,25 @@ var app6 = new Vue({
         message: '안녕하세요 Vue!'
     }
 })
+
+// todo-item 이름을 가진 컴포넌트를 정의합니다.
+Vue.component('todo-item', {
+    // template: '<li>할일 항목 하나입니다.</li>'
+    
+    // 이제 todo-item 컴포넌트는 "prop" 이라고 하는
+    // 사용자 정의 속성 같은 것을 입력받을 수 있습니다.
+    // 이 prop은 todo라는 이름으로 정의했습니다. 
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
